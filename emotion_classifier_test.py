1dcaf3d2e42bb36bb5c58985e510d9dcb48ab720
ohjiwoong <ohjiwoong@gmail.com>
2025-08-06 12:20:19 +0900

    [remove] test file 제거

--- emotion_classifier_test.py
+++
@@ -1,7 +0,0 @@
-from emotion_classifier.emotion_classifier_manager import EmotionClassifierManager
-from emotion_classifier.emotion_classifier_selector import EmotionClassifierSelector
-
-if __name__ == "__main__":
-    classifer = EmotionClassifierSelector.get_emotion_classifier("minilm")
-    manager = EmotionClassifierManager(classifer)
-    print(manager.process("misterious"))
--- ocr_test.py
+++
@@ -1,7 +0,0 @@
-from ocr.ocr_selector import OCRSelector
-from ocr.ocr_manager import OCRManager
-
-if __name__ == "__main__":
-    ocr = OCRSelector.get_reader('easyocr')
-    manager = OCRManager(ocr)
-    print(manager.process("sample_image.png"))
